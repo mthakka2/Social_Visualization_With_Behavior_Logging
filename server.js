0ebd3a8e06872ebeeca46c5c6ead5819a9fdde49
Maitreyi Thakkar <mthakka2@asu.edu>
2018-09-20 01:52:00 -0700

    final commit

--- server.js
+++ server.js
@@ -180,10 +180,10 @@ app.post('/register', (req,res) => {
 
 if(process.env.NODE_ENV === "production"){
 	const path = require("path");
-	app.use(express.static(path.join(_dirname, "/client/build")));
+	app.use(express.static(path.join(__dirname, "/client/build")));
 
 	app.get("*", (req,res) => {
-		res.sendFile(path.resolve(_dirname, "client", "build", "index.html"));
+		res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
 	})
 }
 
