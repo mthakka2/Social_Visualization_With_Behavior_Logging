583a958a77fe285735a8fa5cfcaf25f2e0f5edd5
Maitreyi Thakkar <mthakka2@asu.edu>
2018-09-20 02:33:49 -0700

    adding database

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const moment = require('moment');
 const db = knex({
 	client: 'pg',
 	connection: {
-		host: process.env.DATABASE_URL,
+		connectionString: process.env.DATABASE_URL,
 		ssl:true
 	}
 });
