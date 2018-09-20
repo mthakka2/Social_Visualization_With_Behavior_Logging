e2af05d48cfc2eec2a1d0b9ddfbdca4aeeb19d71
Maitreyi Thakkar <mthakka2@asu.edu>
2018-09-20 00:44:25 -0700

    adding port

--- server.js
+++ server.js
@@ -178,6 +178,6 @@ app.post('/register', (req,res) => {
 	.catch(err => res.status(400).json("not register"))
 	})
 
-app.listen(3000, () => {
-	console.log('app is running on port 3000');
+app.listen(process.env.PORT || 3000, () => {
+	console.log(`app is running on port ${process.env.PORT}`);
 })
